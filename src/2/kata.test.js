5b8fd990ddd3dfd7c9a4a683c27723c1bc0b9081
Matteo Barone <barmat90@gmail.com>
2021-01-25 11:04:36 +0100

    refactor(2): unified sum test in one

--- src/2/kata.test.js
+++ src/2/kata.test.js
@@ -7,23 +7,18 @@ describe.only('add()', () => {
     expect(result).to.equal(0);
   })
 
-  it('should return the number from the string in input, if one', () => {
+  it('should return the sum of the number in input separated by comma, if two', () => {
     const result1 = add('1');
     const result2 = add('45');
-    const result3 = add('2361');
+    const result3 = add('1,2');
+    const result4 = add('2361,7832');
+    const result5 = add('1,2,3,4');
+    const result6 = add('0,0,0,0,0,1,0');
     expect(result1).to.equal(1);
     expect(result2).to.equal(45);
-    expect(result3).to.equal(2361);
-  })
-
-  it('should return the sum of the number in input separated by comma, if two', () => {
-    const result1 = add('1,2');
-    const result2 = add('2361,7832');
-    const result3 = add('1,2,3,4');
-    const result4 = add('0,0,0,0,0,1,0');
-    expect(result1).to.equal(3);
-    expect(result2).to.equal(10193);
-    expect(result3).to.equal(10);
-    expect(result4).to.equal(1);
+    expect(result3).to.equal(3);
+    expect(result4).to.equal(10193);
+    expect(result5).to.equal(10);
+    expect(result6).to.equal(1);
   })
 })
