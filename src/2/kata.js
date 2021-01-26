87199e2c2ce29239bf4cf70604dc2e9dbe552eca
Matteo Barone <barmat90@gmail.com>
2021-01-26 15:40:46 +0100

    feat(2): point 3 and 4

--- src/2/kata.js
+++ src/2/kata.js
@@ -3,7 +3,19 @@ function add(numbers = '') {
     return 0;
   }
 
-  return numbers.split(',').reduce((acc, strNum) => acc + parseInt(strNum), 0)
+  let delimiter = ',';
+  const hasCustomDelimiter = numbers.substr(0, 2) === '//';
+
+  if (hasCustomDelimiter) {
+    delimiter = numbers[2];
+    numbers = numbers.substr(4, numbers.length);
+  }
+
+  const withoutNewLines = numbers.replace(/\n/g, delimiter);
+
+  return withoutNewLines
+    .split(delimiter)
+    .reduce((acc, strNum) => acc + parseInt(strNum), 0)
 }
 
 module.exports = {
--- src/2/kata.test.js
+++ src/2/kata.test.js
@@ -7,18 +7,25 @@ describe('add()', () => {
     expect(result).to.equal(0);
   })
 
-  it('should return the sum of the number in input separated by comma, if two', () => {
+  it('should return the sum of the number in input separated by comma or new lines', () => {
     const result1 = add('1');
-    const result2 = add('45');
-    const result3 = add('1,2');
-    const result4 = add('2361,7832');
-    const result5 = add('1,2,3,4');
-    const result6 = add('0,0,0,0,0,1,0');
+    const result2 = add('2361,7832');
+    const result3 = add('1,2,3,4');
+    const result4 = add('1\n2,3');
+    const result5 = add('2361\n7832\n333');
     expect(result1).to.equal(1);
-    expect(result2).to.equal(45);
-    expect(result3).to.equal(3);
-    expect(result4).to.equal(10193);
-    expect(result5).to.equal(10);
-    expect(result6).to.equal(1);
+    expect(result2).to.equal(10193);
+    expect(result3).to.equal(10);
+    expect(result4).to.equal(6);
+    expect(result5).to.equal(10526);
+  })
+
+  it('should accept custom delimiter if defined in first line', () => {
+    const result1 = add('//;\n1;2');
+    const result2 = add('//%\n1%2%33\n7');
+    const result3 = add('//$\n1\n2\n9');
+    expect(result1).to.equal(3);
+    expect(result2).to.equal(43);
+    expect(result3).to.equal(12);
   })
 })
